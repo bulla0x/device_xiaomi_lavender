e435a56746cb110dd6daaa04877e7cf329824880
bulla0x <jyotishmanbaruah67@gmail.com>
2026-01-13 00:22:49 +0530

    lavender: removed more app
    Signed-off-by: bulla0x <jyotishmanbaruah67@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := YouTube Chrome64 Drive Gmail2 Photos Accord 
+LOCAL_OVERRIDES_PACKAGES := YouTube Chrome-Stub Drive Gmail2 Photos Accord
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
